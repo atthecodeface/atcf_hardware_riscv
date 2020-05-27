10eb946f9d3514d2c4842377ac19c8cc62715a92
Gavin Stark <gjstark@me.com>
2020-05-27 18:43:54 +0100

    [lib] Removed module which has moved to reve_r

---
+++ library_desc.py
@@ -0,0 +1,19 @@
+import cdl_desc
+from cdl_desc import CdlModule, CdlSimVerilatedModule, CModel, CSrc
+
+class Library(cdl_desc.Library):
+    name="riscv"
+    pass
+
+class ReveR(cdl_desc.Modules):
+    name = "reve_r"
+    c_src_dir    = "cmodel"
+    src_dir      = "cdl"
+    tb_src_dir   = "tb_cdl"
+    libraries = {"std":True, "apb":True, "video":True, "utils":True, "jtag":True, "ethernet":True}
+    cdl_include_dirs = ["cdl"]
+    export_dirs = cdl_include_dirs + [ src_dir ]
+    modules = []
+    # modules += [ CdlModule("reve_r_alu") ]
+    pass
+
